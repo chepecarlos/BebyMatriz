ced5cdf63bbe61336f0c79fbabce7c1c546a19d7
chepecarlos <chepecarlos@alswblog.org>
2013-09-04 23:28:55 -0600

    Alargando el espacio para los nuevo caccateres

--- BebyMatriz.ino
+++ BebyMatriz.ino
@@ -10,7 +10,7 @@
 #define LEDMAT_FILA_3 6
 #define LEDMAT_FILA_4 7
 
-const boolean alfabeto[28*5][5] = {
+const boolean alfabeto[40*5][5] = {
   { 0, 1, 1, 0 },  //A
   { 1, 0, 0, 1 },
   { 1, 1, 1, 1 },
@@ -236,7 +236,7 @@ const boolean alfabeto[28*5][5] = {
 };
 
 
-const int anchoCaracter[30] = {
+const int anchoCaracter[40] = {
   4, 4, 4, 4, 4, 4, 4, 4, 3, 4, 4, 4, 5, 4, 4, 4, 4, 4, 4, 3, 4, 4, 5, 4, 3, 4, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1
 //A, B, C, D, E, F, G, H, I, J, K, L, M, N, 0, P, Q, R, S, T, U, V, W, X, Y, Z, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ""
 //0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36
